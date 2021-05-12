30c7fbb965552ad5a8d9e9be9090dad8ff95986a
nabeelmehmood789 <nabeel@rebeltechnology.io>
2021-05-13 02:17:22 +0500

    Working on JWT AUTH

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -6,21 +6,27 @@ import {TasksStatus} from "./task-status.enum";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-    async getTask(filterDto: GetTasksFilterDto) : Promise<Task[]> {
-        const {status,search} = filterDto;
+    async getTask(filterDto: GetTasksFilterDto): Promise<Task[]> {
+        const {status, search} = filterDto;
         const query = this.createQueryBuilder('task');
+        if(search){
+            query.andWhere("(task.title LIKE :search OR task.description LIKE :search)",{search: `%${search}%`})
+        }
+        if (status) {
+            query.andWhere('task.status = :status', {status})
+        }
+
         const tasks = await query.getMany();
         return tasks;
     }
 
-    async createTask(createTaskDto: CreateTaskDto) : Promise<Task>{
-        const {title,description}  = createTaskDto;
+    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+        const {title, description} = createTaskDto;
         const task = new Task();
         task.title = title;
         task.description = description;
         task.status = TasksStatus.OPEN;
-        await task.save();
+        task.save();
         return task;
-
     }
 }
